4941cdc8d09334c3b28980f8a103331151b3246b
blablupcom <o.volodin@yahoo.com>
2018-06-20 14:48:53 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@
 
 import os
 import re
+import json
 import scraperwiki
 import urllib2
 from datetime import datetime
@@ -36,21 +37,21 @@ def validateFilename(filename):
         return True
 
 
-def validateURL(url):
+def validateURL(url, requestdata):
     try:
-        r = urllib2.urlopen(url)
+        r = requests.get(url, params=requestdata)
         count = 1
-        while r.getcode() == 500 and count < 4:
+        while r.status_code == 500 and count < 4:
             print ("Attempt {0} - Status code: {1}. Retrying.".format(count, r.status_code))
             count += 1
-            r = urllib2.urlopen(url)
+            r = requests.get(url, params=requestdata)
         sourceFilename = r.headers.get('Content-Disposition')
 
         if sourceFilename:
             ext = os.path.splitext(sourceFilename)[1].replace('"', '').replace(';', '').replace(' ', '')
         else:
             ext = os.path.splitext(url)[1]
-        validURL = r.getcode() == 200
+        validURL = r.status_code == 200
         validFiletype = ext.lower() in ['.csv', '.xls', '.xlsx']
         return validURL, validFiletype
     except:
@@ -58,9 +59,9 @@ def validateURL(url):
         return False, False
 
 
-def validate(filename, file_url):
+def validate(filename, file_url, requestdata):
     validFilename = validateFilename(filename)
-    validURL, validFiletype = validateURL(file_url)
+    validURL, validFiletype = validateURL(file_url, requestdata)
     if not validFilename:
         print filename, "*Error: Invalid filename*"
         print file_url
@@ -85,51 +86,51 @@ def convert_mth_strings ( mth_string ):
 
 #### VARIABLES 1.0
 
-entity_id = "E2632_BDC_gov"
-url = "https://www.broadland.gov.uk/info/200197/spending_and_transparency/339/council_spending_over_250"
+entity_id = "E1932_DBC_gov"
+url = "http://webapps.dacorum.gov.uk/spendingdata/spendingdownload.aspx"
 errors = 0
 data = []
-
-
+months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+year_dic = {'2018':{"valid":"%2FwEdABSE8Qt1KlV1J9KeNPIQtTBPE5bA6PjsYbPvOGJjNwlyAa9S5EB8MvMvEr2bCY19cFmJ7WszDpJU6T1weIR4Ie1xxBSZ60%2FHerXDuuv9taVs1QXOQ9MO7e4yLWykoNwhhxGNPl%2B9JJt5%2FrGRi6uIhxF9pFpM53q34CsSY8OiQWOe69rGvmEnaVCrmt7ClYfqyqXHwLNZx5kDV%2BEZj13QqaspCFmVeQ39T1K%2Fb9iPIPYfU2HtwfH%2Fc9y7GjG%2BtOcSWJ7EU2JFh7Ixy6El%2BG5XPxZ2SAncKJa2uhrJcYJt61rVn1eMEqCqplFVx56mfisa7fF3PAMvcIeAZxY%2FKu%2Fu9kT0YoThdbZr%2B2CTJLc00jsYikxTT0xjzFpB7dY1OC5GpNT53d8HLcsMPPSWw23i74K4VDN27krvfpm%2B1IzZ4LdVW4aAZpU%2BpFFMPBtJ49kNKFEDXJelKNaub9Od9FVL%2FM4W", "state":"%2FwEPDwUJODMzNTE1NDU0D2QWAmYPZBYCAgMPZBYEAgkPFgIeBFRleHQFHzxoMT5TcGVuZGluZyBEYXRhIERvd25sb2FkPC9oMT5kAg4PZBYCAhEPD2QPEBYBZhYBFgIeDlBhcmFtZXRlclZhbHVlBQQyMDE4FgFmZGQYAQUeX19Db250cm9sc1JlcXVpcmVQb3N0QmFja0tleV9fFgEFDGN0bDAwJHNlYXJjaHObYZajbV0Qqcyx81vTtkJoyVi1ksSL99cql7PLu055"},
+            '2017':{"valid": "%2FwEdABtnlVX4p2%2FeGG%2FBk%2FEpzCPNE5bA6PjsYbPvOGJjNwlyAa9S5EB8MvMvEr2bCY19cFmJ7WszDpJU6T1weIR4Ie1xxBSZ60%2FHerXDuuv9taVs1QXOQ9MO7e4yLWykoNwhhxGNPl%2B9JJt5%2FrGRi6uIhxF9pFpM53q34CsSY8OiQWOe69rGvmEnaVCrmt7ClYfqyqXHwLNZx5kDV%2BEZj13QqaspCFmVeQ39T1K%2Fb9iPIPYfU2HtwfH%2Fc9y7GjG%2BtOcSWJ7EU2JFh7Ixy6El%2BG5XPxZ2SAncKJa2uhrJcYJt61rVn1eMEqCqplFVx56mfisa7fF3PAMvcIeAZxY%2FKu%2Fu9kT0YoThdbZr%2B2CTJLc00jsYikxTT0xjzFpB7dY1OC5GpNRJ%2B7R00ez4JKanodeClR32CIbeuunBml1Q0tx3733lvDmQX8UjihxKztmYmU%2FKDsNwgBghZwAWAIHnn6u%2FHfqXL0%2BkWkQaS83mdW4Xqd7SPZXhbjDQNwkl%2FjO7SMWeursHdfyFhq4xwx0bswivNszN%2Bd3fBy3LDDz0lsNt4u%2BCuFQzdu5K736ZvtSM2eC3VVvL7TedW2T%2BbjuwigJBdGW6cFe5149fu5hFHShNbSBe2A%3D%3D", "state":"%2FwEPDwUJODMzNTE1NDU0D2QWAmYPZBYCAgMPZBYEAgkPFgIeBFRleHQFHzxoMT5TcGVuZGluZyBEYXRhIERvd25sb2FkPC9oMT5kAg4PZBYCAhEPD2QPEBYBZhYBFgIeDlBhcmFtZXRlclZhbHVlBQQyMDE2FgFmZGQYAQUeX19Db250cm9sc1JlcXVpcmVQb3N0QmFja0tleV9fFgEFDGN0bDAwJHNlYXJjaC47UVGwiDjYbQkn9lBWy0zIQ9i%2Bm0r86kfpFkvmSU5U"},
+            '2016':{"valid":"%2FwEdABtnlVX4p2%2FeGG%2FBk%2FEpzCPNE5bA6PjsYbPvOGJjNwlyAa9S5EB8MvMvEr2bCY19cFmJ7WszDpJU6T1weIR4Ie1xxBSZ60%2FHerXDuuv9taVs1QXOQ9MO7e4yLWykoNwhhxGNPl%2B9JJt5%2FrGRi6uIhxF9pFpM53q34CsSY8OiQWOe69rGvmEnaVCrmt7ClYfqyqXHwLNZx5kDV%2BEZj13QqaspCFmVeQ39T1K%2Fb9iPIPYfU2HtwfH%2Fc9y7GjG%2BtOcSWJ7EU2JFh7Ixy6El%2BG5XPxZ2SAncKJa2uhrJcYJt61rVn1eMEqCqplFVx56mfisa7fF3PAMvcIeAZxY%2FKu%2Fu9kT0YoThdbZr%2B2CTJLc00jsYikxTT0xjzFpB7dY1OC5GpNRJ%2B7R00ez4JKanodeClR32CIbeuunBml1Q0tx3733lvDmQX8UjihxKztmYmU%2FKDsNwgBghZwAWAIHnn6u%2FHfqXL0%2BkWkQaS83mdW4Xqd7SPZXhbjDQNwkl%2FjO7SMWeursHdfyFhq4xwx0bswivNszN%2Bd3fBy3LDDz0lsNt4u%2BCuFQzdu5K736ZvtSM2eC3VVvL7TedW2T%2BbjuwigJBdGW6cFe5149fu5hFHShNbSBe2A%3D%3D","state":"%2FwEPDwUJODMzNTE1NDU0D2QWAmYPZBYCAgMPZBYEAgkPFgIeBFRleHQFHzxoMT5TcGVuZGluZyBEYXRhIERvd25sb2FkPC9oMT5kAg4PZBYCAhEPD2QPEBYBZhYBFgIeDlBhcmFtZXRlclZhbHVlBQQyMDE2FgFmZGQYAQUeX19Db250cm9sc1JlcXVpcmVQb3N0QmFja0tleV9fFgEFDGN0bDAwJHNlYXJjaC47UVGwiDjYbQkn9lBWy0zIQ9i%2Bm0r86kfpFkvmSU5U"},
+            '2015':{"valid":"%2FwEdABvh3jfypoqVq%2FhjFAx49uQtE5bA6PjsYbPvOGJjNwlyAa9S5EB8MvMvEr2bCY19cFmJ7WszDpJU6T1weIR4Ie1xxBSZ60%2FHerXDuuv9taVs1QXOQ9MO7e4yLWykoNwhhxGNPl%2B9JJt5%2FrGRi6uIhxF9pFpM53q34CsSY8OiQWOe69rGvmEnaVCrmt7ClYfqyqXHwLNZx5kDV%2BEZj13QqaspCFmVeQ39T1K%2Fb9iPIPYfU2HtwfH%2Fc9y7GjG%2BtOcSWJ7EU2JFh7Ixy6El%2BG5XPxZ2SAncKJa2uhrJcYJt61rVn1eMEqCqplFVx56mfisa7fF3PAMvcIeAZxY%2FKu%2Fu9kT0YoThdbZr%2B2CTJLc00jsYikxTT0xjzFpB7dY1OC5GpNRJ%2B7R00ez4JKanodeClR32CIbeuunBml1Q0tx3733lvDmQX8UjihxKztmYmU%2FKDsNwgBghZwAWAIHnn6u%2FHfqXL0%2BkWkQaS83mdW4Xqd7SPZXhbjDQNwkl%2FjO7SMWeursHdfyFhq4xwx0bswivNszN%2Bd3fBy3LDDz0lsNt4u%2BCuFQzdu5K736ZvtSM2eC3VVtgxNka8Zl%2BSn4ozbabRVDCAL79U8jbqK9QjofmztK2Sw%3D%3D","state":"%2FwEPDwUJODMzNTE1NDU0D2QWAmYPZBYCAgMPZBYEAgkPFgIeBFRleHQFHzxoMT5TcGVuZGluZyBEYXRhIERvd25sb2FkPC9oMT5kAg4PZBYCAhEPD2QPEBYBZhYBFgIeDlBhcmFtZXRlclZhbHVlBQQyMDE1FgFmZGQYAQUeX19Db250cm9sc1JlcXVpcmVQb3N0QmFja0tleV9fFgEFDGN0bDAwJHNlYXJjaGrtYmL7qxo%2Ffzw%2BDv6vtDC0zSj0q1cPvlIoSwQOAKNv"},
+            '2014':{"valid":"%2FwEdABtiS4XMQKH2io4Gup0gZnofE5bA6PjsYbPvOGJjNwlyAa9S5EB8MvMvEr2bCY19cFmJ7WszDpJU6T1weIR4Ie1xxBSZ60%2FHerXDuuv9taVs1QXOQ9MO7e4yLWykoNwhhxGNPl%2B9JJt5%2FrGRi6uIhxF9pFpM53q34CsSY8OiQWOe69rGvmEnaVCrmt7ClYfqyqXHwLNZx5kDV%2BEZj13QqaspCFmVeQ39T1K%2Fb9iPIPYfU2HtwfH%2Fc9y7GjG%2BtOcSWJ7EU2JFh7Ixy6El%2BG5XPxZ2SAncKJa2uhrJcYJt61rVn1eMEqCqplFVx56mfisa7fF3PAMvcIeAZxY%2FKu%2Fu9kT0YoThdbZr%2B2CTJLc00jsYikxTT0xjzFpB7dY1OC5GpNRJ%2B7R00ez4JKanodeClR32CIbeuunBml1Q0tx3733lvDmQX8UjihxKztmYmU%2FKDsNwgBghZwAWAIHnn6u%2FHfqXL0%2BkWkQaS83mdW4Xqd7SPZXhbjDQNwkl%2FjO7SMWeursHdfyFhq4xwx0bswivNszN%2Bd3fBy3LDDz0lsNt4u%2BCuFQzdu5K736ZvtSM2eC3VVt3HgcbU28wNzdhOq396xApmIQ8o2wCEBMs%2FughwIW4zQ%3D%3D","state":"%2FwEPDwUJODMzNTE1NDU0D2QWAmYPZBYCAgMPZBYEAgkPFgIeBFRleHQFHzxoMT5TcGVuZGluZyBEYXRhIERvd25sb2FkPC9oMT5kAg4PZBYCAhEPD2QPEBYBZhYBFgIeDlBhcmFtZXRlclZhbHVlBQQyMDE0FgFmZGQYAQUeX19Db250cm9sc1JlcXVpcmVQb3N0QmFja0tleV9fFgEFDGN0bDAwJHNlYXJjaO2IZM5AtwTBarCAn%2B0egsy%2BINEZVVLjY%2FnK8Izl2QU9"},
+            '2013':{"valid":"%2FwEdABumfXN4ROuHGAOjuz%2BUkbsYE5bA6PjsYbPvOGJjNwlyAa9S5EB8MvMvEr2bCY19cFmJ7WszDpJU6T1weIR4Ie1xxBSZ60%2FHerXDuuv9taVs1QXOQ9MO7e4yLWykoNwhhxGNPl%2B9JJt5%2FrGRi6uIhxF9pFpM53q34CsSY8OiQWOe69rGvmEnaVCrmt7ClYfqyqXHwLNZx5kDV%2BEZj13QqaspCFmVeQ39T1K%2Fb9iPIPYfU2HtwfH%2Fc9y7GjG%2BtOcSWJ7EU2JFh7Ixy6El%2BG5XPxZ2SAncKJa2uhrJcYJt61rVn1eMEqCqplFVx56mfisa7fF3PAMvcIeAZxY%2FKu%2Fu9kT0YoThdbZr%2B2CTJLc00jsYikxTT0xjzFpB7dY1OC5GpNRJ%2B7R00ez4JKanodeClR32CIbeuunBml1Q0tx3733lvDmQX8UjihxKztmYmU%2FKDsNwgBghZwAWAIHnn6u%2FHfqXL0%2BkWkQaS83mdW4Xqd7SPZXhbjDQNwkl%2FjO7SMWeursHdfyFhq4xwx0bswivNszN%2Bd3fBy3LDDz0lsNt4u%2BCuFQzdu5K736ZvtSM2eC3VVscx5HtgQG1N%2BfENq39F6MXB9QnCL1H7ZIChFQcPYZVTA%3D%3D","state":"%2FwEPDwUJODMzNTE1NDU0D2QWAmYPZBYCAgMPZBYEAgkPFgIeBFRleHQFHzxoMT5TcGVuZGluZyBEYXRhIERvd25sb2FkPC9oMT5kAg4PZBYCAhEPD2QPEBYBZhYBFgIeDlBhcmFtZXRlclZhbHVlBQQyMDEzFgFmZGQYAQUeX19Db250cm9sc1JlcXVpcmVQb3N0QmFja0tleV9fFgEFDGN0bDAwJHNlYXJjaJ%2F5sNrlgKHz0ybEy6LR1XzLnhVsqRSmUZebIo34YOrH"},
+            '2012':{"valid":"%2FwEdABuT7HjAHemp9ygUJ0rA8duiE5bA6PjsYbPvOGJjNwlyAa9S5EB8MvMvEr2bCY19cFmJ7WszDpJU6T1weIR4Ie1xxBSZ60%2FHerXDuuv9taVs1QXOQ9MO7e4yLWykoNwhhxGNPl%2B9JJt5%2FrGRi6uIhxF9pFpM53q34CsSY8OiQWOe69rGvmEnaVCrmt7ClYfqyqXHwLNZx5kDV%2BEZj13QqaspCFmVeQ39T1K%2Fb9iPIPYfU2HtwfH%2Fc9y7GjG%2BtOcSWJ7EU2JFh7Ixy6El%2BG5XPxZ2SAncKJa2uhrJcYJt61rVn1eMEqCqplFVx56mfisa7fF3PAMvcIeAZxY%2FKu%2Fu9kT0YoThdbZr%2B2CTJLc00jsYikxTT0xjzFpB7dY1OC5GpNRJ%2B7R00ez4JKanodeClR32CIbeuunBml1Q0tx3733lvDmQX8UjihxKztmYmU%2FKDsNwgBghZwAWAIHnn6u%2FHfqXL0%2BkWkQaS83mdW4Xqd7SPZXhbjDQNwkl%2FjO7SMWeursHdfyFhq4xwx0bswivNszN%2Bd3fBy3LDDz0lsNt4u%2BCuFQzdu5K736ZvtSM2eC3VVvaAYcI%2B0usNvIdr6CAYJ8rcc98ilKcSANXjIY3q38JrQ%3D%3D","state":"%2FwEPDwUJODMzNTE1NDU0D2QWAmYPZBYCAgMPZBYEAgkPFgIeBFRleHQFHzxoMT5TcGVuZGluZyBEYXRhIERvd25sb2FkPC9oMT5kAg4PZBYCAhEPD2QPEBYBZhYBFgIeDlBhcmFtZXRlclZhbHVlBQQyMDEyFgFmZGQYAQUeX19Db250cm9sc1JlcXVpcmVQb3N0QmFja0tleV9fFgEFDGN0bDAwJHNlYXJjaE9LCarSM8TfqpVC77d8pN84AaCWKyZSSDzivPltuqGk"},
+            '2011':{"valid":"%2FwEdABtlKV%2BYSw6B3mQ7SPthvfT6E5bA6PjsYbPvOGJjNwlyAa9S5EB8MvMvEr2bCY19cFmJ7WszDpJU6T1weIR4Ie1xxBSZ60%2FHerXDuuv9taVs1QXOQ9MO7e4yLWykoNwhhxGNPl%2B9JJt5%2FrGRi6uIhxF9pFpM53q34CsSY8OiQWOe69rGvmEnaVCrmt7ClYfqyqXHwLNZx5kDV%2BEZj13QqaspCFmVeQ39T1K%2Fb9iPIPYfU2HtwfH%2Fc9y7GjG%2BtOcSWJ7EU2JFh7Ixy6El%2BG5XPxZ2SAncKJa2uhrJcYJt61rVn1eMEqCqplFVx56mfisa7fF3PAMvcIeAZxY%2FKu%2Fu9kT0YoThdbZr%2B2CTJLc00jsYikxTT0xjzFpB7dY1OC5GpNRJ%2B7R00ez4JKanodeClR32CIbeuunBml1Q0tx3733lvDmQX8UjihxKztmYmU%2FKDsNwgBghZwAWAIHnn6u%2FHfqXL0%2BkWkQaS83mdW4Xqd7SPZXhbjDQNwkl%2FjO7SMWeursHdfyFhq4xwx0bswivNszN%2Bd3fBy3LDDz0lsNt4u%2BCuFQzdu5K736ZvtSM2eC3VVtVmw7wehnuag1yOc8r6ZPHnxbs3HnuPhYP0pnK9ekt1Q%3D%3D","state":"%2FwEPDwUJODMzNTE1NDU0D2QWAmYPZBYCAgMPZBYEAgkPFgIeBFRleHQFHzxoMT5TcGVuZGluZyBEYXRhIERvd25sb2FkPC9oMT5kAg4PZBYCAhEPD2QPEBYBZhYBFgIeDlBhcmFtZXRlclZhbHVlBQQyMDExFgFmZGQYAQUeX19Db250cm9sc1JlcXVpcmVQb3N0QmFja0tleV9fFgEFDGN0bDAwJHNlYXJjaF62uz4npyC0eg0BshNwTSlcUI3N6Hh3pYB8Tf%2FrEwBo"},
+            '2010':{"valid":"%2FwEdABBXp3PtT06%2BBCP7zkykpPI%2FE5bA6PjsYbPvOGJjNwlyAa9S5EB8MvMvEr2bCY19cFmJ7WszDpJU6T1weIR4Ie1xxBSZ60%2FHerXDuuv9taVs1QXOQ9MO7e4yLWykoNwhhxGNPl%2B9JJt5%2FrGRi6uIhxF9pFpM53q34CsSY8OiQWOe69rGvmEnaVCrmt7ClYfqyqXHwLNZx5kDV%2BEZj13QqaspCFmVeQ39T1K%2Fb9iPIPYfU2HtwfH%2Fc9y7GjG%2BtOcSWJ7EU2JFh7Ixy6El%2BG5XPxZ2B3X8hYauMcMdG7MIrzbMzfnd3wctyww89JbDbeLvgrhUM3buSu9%2Bmb7UjNngt1VbcBx%2FbMYL%2BEYnnVuyG19KuX%2BqEm2J9dr7Ie2PQyyQOn8%3D","state":"%2FwEPDwUJODMzNTE1NDU0D2QWAmYPZBYCAgMPZBYEAgkPFgIeBFRleHQFHzxoMT5TcGVuZGluZyBEYXRhIERvd25sb2FkPC9oMT5kAg4PZBYCAhEPD2QPEBYBZhYBFgIeDlBhcmFtZXRlclZhbHVlBQQyMDEwFgFmZGQYAQUeX19Db250cm9sc1JlcXVpcmVQb3N0QmFja0tleV9fFgEFDGN0bDAwJHNlYXJjaLesOIWM6gDniRWWOs7aXibENAwRCEi3522uQ%2FBsIXEU"}
+            }
 #### READ HTML 1.0
+import requests
 
-html = urllib2.urlopen(url)
-soup = BeautifulSoup(html, 'lxml')
+html = requests.get(url)
+soup = BeautifulSoup(html.text, 'lxml')
 
 #### SCRAPE DATA
 
-links = soup.find('div', 'editor').find_all('a', href=True)
-for link in links:
-    if 'http' not in link['href']:
-        year_url = 'https://www.broadland.gov.uk' + link['href']
-    else:
-        year_url = link['href']
-    year_html = urllib2.urlopen(year_url)
-    year_soup = BeautifulSoup(year_html, 'lxml')
-    blocks = year_soup.find_all('span', 'download-listing__file-tag download-listing__file-tag--type')
-    for block in blocks:
-        if 'CSV' in block.text:
-            url = block.find_next('a')['href']
-            if 'http' not in url:
-                url = 'https://www.broadland.gov.uk' + url
-            else:
-                url = url
-            file_name = block.find_next('a')['aria-label']
-            csvMth = file_name.split()[-2][:3]
-            csvYr = file_name.split()[-1]
-            csvMth = convert_mth_strings(csvMth.upper())
-            data.append([csvYr, csvMth, url])
+d = """__EVENTTARGET=&__EVENTARGUMENT=&__LASTFOCUS=&__VIEWSTATE={}&__VIEWSTATEGENERATOR=7D80A3C7&__EVENTVALIDATION={}&ctl00%24searchBox=&ctl00%24MainContent%24ddYYYY={}&ctl00%24MainContent%24ddMMM={}&ctl00%24MainContent%24Button1=&ctl00%24FileCounter=0"""
+for k, v in year_dic.items():
+    for month in months:
+        val = v['valid']
+        s = v['state']
+        csvMth = month
+        csvYr = k
+        dat = d.format(s, val, k, month)
+        html = requests.get(url, params=dat)
+        if html.status_code == 200:
+            data.append([csvYr, csvMth, url, dat])
 
 
 #### STORE DATA 1.0
 
 for row in data:
-    csvYr, csvMth, url = row
+    csvYr, csvMth, url, requestdata = row
     filename = entity_id + "_" + csvYr + "_" + csvMth
     todays_date = str(datetime.now())
     file_url = url.strip()
 
-    valid = validate(filename, file_url)
+    valid = validate(filename, file_url, requestdata)
 
     if valid == True:
         scraperwiki.sqlite.save(unique_keys=['l'], data={"l": file_url, "f": filename, "d": todays_date })
